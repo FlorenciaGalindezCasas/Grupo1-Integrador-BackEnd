9ff2c1da259f30e644a39d66b88bf46fa8fa3d92
Juan <JLische-@hotmail.com>
2023-12-07 16:40:43 -0300

    [FEAT] getShopView y getCartView features de shopController

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,33 +1,37 @@
 const path = require("path");
 const fs = require("fs");
 
+function reedJsonFile(callback) {
+  fs.readFile('db.json', 'utf8', (err, data) => {
+    if (err) throw new Error('Error al leer el archivo JSON:', err);
+
+    try {
+      const objetoJSON = JSON.parse(data);
+      console.log('Contenido del archivo JSON:', objetoJSON);
+      callback(null, objetoJSON);
+    } catch (error) {
+      throw new Error('Error al analizar el JSON:', error);
+    }
+  });
+}
+
 const shopController = {
   getShopView: (req, res) => {
-    fs.readFile('db.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error('Error al leer el archivo JSON:', err);
-        return;
-      }
-    
-      console.log('Contenido del archivo JSON:', data);
-    
-      try {
-        const objetoJSON = JSON.parse(data);
-        return res.render(path.resolve(__dirname, "../views/shop"), {
-          items: objetoJSON
-        })
-        console.log('Objeto JSON:', objetoJSON);
-      } catch (error) {
-        console.error('Error al analizar el JSON:', error);
-      }
-    });
-    /* return res.render(path.resolve(__dirname, "../views/shop"), {
-      items: itemsParsed
-    }); */
+    reedJsonFile((err, result) => {
+      return res.render(path.resolve(__dirname, "../views/shop"), {
+        items: result
+      })
+    })
   },
   getItem: (req, res) => res.send(`Ruta para obtener el item de ID: ${req.params.id}`),
   addItemToCart: (req, res) => res.send(`Ruta para agregar el item de ID ${req.params.id} al carrito`),
-  getCartView: (req, res) => res.send("Ruta para obtener la vista del carrito"),
+  getCartView: (req, res) => {
+    reedJsonFile((err, result) => {
+      return res.render(path.resolve(__dirname, "../views/carrito"), {
+        items: result
+      })
+    })
+  },
   confirmPurchase: (req, res) => res.send("Ruta para confirmar la compra")
 };
 
