ffeff9aa3098d69a5a0c335fec938dbbdb8c4b79
Juan <JLische-@hotmail.com>
2023-12-09 18:13:22 -0300

    [FEAT] create de productos

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,7 +1,38 @@
+const Product = require('../models/product.model');
+
 const adminController = {
   getAdminView: (req, res) => res.send("Pagina de admin"),
   getCreateView: (req, res) => res.send("Pagina de creacion de admins"),
-  create: (req, res) => res.send("Ruta para confirmar la creacion de un admin"),
+  create: async (req, res) => {
+    const { 
+      product_name,
+      product_description,
+      price,
+      stock,
+      discount,
+      sku,
+      dues,
+      image_front,
+      image_back,
+      license_id,
+      category_id
+    } = req.body;
+
+    const newProduct = await Product.create({
+      product_name,
+      product_description,
+      price,
+      stock,
+      discount,
+      sku,
+      dues,
+      image_front,
+      image_back,
+      license_id,
+      category_id
+    });
+    return newProduct;
+  },
   getUpdateView: (req, res) => res.send(`Pagina de edicion de admin con ID: ${req.params.id}`),
   update: (req, res) => res.send(`Ruta para confirmar la edicion del admin con ID: ${req.params.id}`),
   delete: (req, res) => res.send(`Ruta para confirmar la eliminacion del admin con ID: ${req.params.id}`),
