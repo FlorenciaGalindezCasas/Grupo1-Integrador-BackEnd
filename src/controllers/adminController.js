becad91ad76e58fd66098fe58369547dde9bfb31
Juan <JLische-@hotmail.com>
2023-12-09 23:08:03 -0300

    renegando con las relaciones

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -4,34 +4,41 @@ const adminController = {
   getAdminView: (req, res) => res.send("Pagina de admin"),
   getCreateView: (req, res) => res.send("Pagina de creacion de admins"),
   create: async (req, res) => {
-    const { 
-      product_name,
-      product_description,
-      price,
-      stock,
-      discount,
-      sku,
-      dues,
-      image_front,
-      image_back,
-      license_id,
-      category_id
-    } = req.body;
+    try {
+      const { 
+        product_name,
+        product_description,
+        price,
+        stock,
+        discount,
+        sku,
+        dues,
+        image_front,
+        image_back,
+        license_id,
+        category_id
+      } = req.body;
+  
+      const newProduct = await Product.create({
+        product_name,
+        product_description,
+        price,
+        stock,
+        discount,
+        sku,
+        dues,
+        image_front,
+        image_back,
+        license_id,
+        category_id
+      });
 
-    const newProduct = await Product.create({
-      product_name,
-      product_description,
-      price,
-      stock,
-      discount,
-      sku,
-      dues,
-      image_front,
-      image_back,
-      license_id,
-      category_id
-    });
-    return newProduct;
+      console.log('Producto: ', newProduct);
+
+      return res.status(201).json({ product: newProduct });
+    } catch (error) {
+      return res.status(500).send('Internal server error');
+    }
   },
   getUpdateView: (req, res) => res.send(`Pagina de edicion de admin con ID: ${req.params.id}`),
   update: (req, res) => res.send(`Ruta para confirmar la edicion del admin con ID: ${req.params.id}`),
--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,34 +1,34 @@
 const fs = require("fs");
-
-function reedJsonFile(callback) {
-  fs.readFile('db.json', 'utf8', (err, data) => {
-    if (err) throw new Error('Error al leer el archivo JSON:', err);
-
-    try {
-      const objetoJSON = JSON.parse(data);
-      console.log('Contenido del archivo JSON:', objetoJSON);
-      callback(null, objetoJSON);
-    } catch (error) {
-      throw new Error('Error al analizar el JSON:', error);
-    }
-  });
-}
+const Product = require('../models/product.model');
+const Category = require('../models/category.model');
+const License = require('../models/license.model');
 
 const shopController = {
-  getShopView: (req, res) => {
-    reedJsonFile((err, result) => {
-      return res.render("shop", {
-        items: result
-      })
-    })
-  },
-  getItem: (req, res) => {
-    reedJsonFile((err, result) => {
-      const item = result.find(i => i.product_id === +req.params.id);
-      return res.render("item", {
-        item
+  getShopView: async (req, res) => {
+    try {
+      const products = await Product.findAll();
+
+      console.log('Producto: ', products);
+
+      return res.status(200).render("shop", {
+        items: products
       });
-    })
+    } catch (error) {
+      return res.status(500).send({error})
+    }
+  },
+  getItem: async (req, res) => {
+    try {
+      const item = await Product.findByPk(req.params.id)
+      if (!item) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+    return res.status(200).render("item", {
+      item 
+    });      
+    } catch (error) {
+      return res.status(500).send("Error")
+    }
   },
   addItemToCart: (req, res) => res.send(`Ruta para agregar el item de ID ${req.params.id} al carrito`),
   getCartView: (req, res) => res.render("carrito"),
