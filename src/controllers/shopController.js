1dac9f1bb194ceefc37086eb7109d54f121b6096
FlorenciaDGC <florenciagalindezcasas@hotmail.com>
2023-12-11 18:39:14 -0300

    arreglos carrito

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -8,96 +8,57 @@ const shopController = {
   getShopView: async (req, res) => {
     try {
       const products = await Product.findAll({
-        include: [License, Category]
+        include: [License, Category],
       });
 
       return res.status(200).render("shop", {
-        items: products
+        items: products,
       });
     } catch (error) {
-      return res.status(500).send({error})
+      return res.status(500).send({ error });
     }
   },
   getItem: async (req, res) => {
     try {
-      const item = await Product.findByPk(req.params.id)
+      const item = await Product.findByPk(req.params.id);
       if (!item) {
-      return res.status(404).json({ error: 'Product not found' });
-    }
-    return res.status(200).render("item", {
-      item 
-    });      
+        return res.status(404).json({ error: "Product not found" });
+      }
+      return res.status(200).render("item", {
+        item,
+      });
     } catch (error) {
-      return res.status(500).send("Error")
+      return res.status(500).send("Error");
     }
   },
   addItemToCart: async (req, res) => {
     try {
       const productID = +req.params.id;
 
-      const cantidad = +req.body.cantidad
-      
-      const product = await Product.findByPk(productID)
+      const cantidad = +req.body.cantidad;
+
+      const product = await Product.findByPk(productID);
       if (!product) {
         return res.status(404).json({ error: "Product not found" });
       }
 
-       const compra = await Cart.create({
+      const compra = await Cart.create({
         product_id: productID,
         cantidad: cantidad,
-        total: cantidad*product.price 
-      })
-      
+        total: cantidad * product.price,
+      });
+
       return res.status(201).json({
         compra,
-        message: "Producto agregado con exito"
-      })
-
-    } catch (error) {
-       console.error(error);
-       return res.status(500).json({ error });
-    }
-  },
-  getCartView: async (req, res) => {
-    try {
-      const compras = await Cart.findAll({
-        include: [
-          {
-            model: Product,
-            include: [License, Category]
-          }
-        ]
-      });
-
-      let cantidad_total = 0;
-      compras.map(item => cantidad_total += item.cantidad);
-
-      let precio_total = 0;
-       
-      compras.map(item => precio_total += +item.total)
-
-      return res.redirect("/cart");
-
-/*       return res.status(200).render("carrito", {
-        items: compras,
-        cantidad_total,
-        precio_total
-      }) */
-    } catch (error) {
-      res.status(500).json({ error });
-    }
-  },
-  confirmPurchase: (req, res) => {
-    try {
-      return res.status(200).render("carrito", {
-        items: null,
-        cantidad_total: 0,
-        precio_total: 0
+        message: "Producto agregado con exito",
       });
     } catch (error) {
-      res.status(500).json({ error });
+      console.error(error);
+      return res.status(500).json({ error });
     }
-  }
+  },
+  getCartView: (req, res) => res.render("carrito"),
+  confirmPurchase: (req, res) => res.render("carrito"),
 };
 
 module.exports = shopController;
