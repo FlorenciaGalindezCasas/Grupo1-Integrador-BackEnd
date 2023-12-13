9c7d44535c39fc9d471a48f696092b6cf9cba300
FlorenciaDGC <florenciagalindezcasas@hotmail.com>
2023-12-13 15:03:25 -0300

    views

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,18 +1,22 @@
 const adminController = {
-  getAdminView: (req, res) => res.send("Pagina de admin"),
-  getCreateView: (req, res) => res.send("Pagina de creacion de admins"),
-  create: async (req, res) => {
-    /* try {
+  getAdminView: (req, res) => res.render("admin", { title: "Admin" }),
+  getCreateView: (req, res) => res.render("create", { title: "Crear" }),
+  create: async (req, res) => {},
+  /* try {
       const newProduct = await Product.create(req.body);
       return res.status(201).json( newProduct );
     } catch (error) {
       console.error(error.message);
       return res.status(500).send('Internal server error');
     } */
-  },
-  getUpdateView: (req, res) => res.send(`Pagina de edicion de admin con ID: ${req.params.id}`),
-  update: (req, res) => res.send(`Ruta para confirmar la edicion del admin con ID: ${req.params.id}`),
-  remove: (req, res) => res.send(`Ruta para confirmar la eliminacion del admin con ID: ${req.params.id}`),
+
+  getUpdateView: (req, res) =>
+    res.render(`Pagina de edicion de admin con ID: ${req.params.id}`),
+  update: (req, res) => res.render("edit", { title: "Editar" }),
+  remove: (req, res) =>
+    res.send(
+      `Ruta para confirmar la eliminacion del admin con ID: ${req.params.id}`
+    ),
 };
 
 module.exports = adminController;
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,9 @@
 const authController = {
-  getLoginView: (req, res) => res.send("Pagina de login"),
+  getLoginView: (req, res) => res.render("login", { title: "Login" }),
   login: (req, res) => res.send("Ruta para loguearse"),
   getRegisterView: (req, res) => res.send("Pagina para registrarse"),
   register: (req, res) => res.send("Ruta para confirmar el registro"),
-  getLogoutView: (req, res) => res.send("Pagina para cerrar sesion?")
+  getLogoutView: (req, res) => res.send("Pagina para cerrar sesion?"),
 };
 
 module.exports = authController;
