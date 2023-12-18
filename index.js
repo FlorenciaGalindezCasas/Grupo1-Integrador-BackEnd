e6b0dfa1e7569f25e57ebf5bcd811bee2d5ad6d6
Juan <JLische-@hotmail.com>
2023-12-17 21:07:32 -0300

    [FEAT] session-store

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
 
 const mainRoutes = require('./src/routes/mainRoutes.js');
 const authRoutes = require('./src/routes/authRoutes.js');
@@ -10,12 +11,23 @@ const shopRoutes = require('./src/routes/shopRoutes.js');
 const { isLogged } = require('./src/middlewares/authorization.js');
 
 const app = express();
+const sessionStore = new MySQLStore({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,  
+	port: process.env.DB_PORT,
+	waitForConnections: true,
+	connectionLimit: 10,  
+	queueLimit: 0
+});
 
 app.use('/public', express.static(path.resolve(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './src/views'));
 app.use(session({
 	secret: 's3cr3TS3ss1oN',
+	store: sessionStore,
 	resave: false,
 	saveUninitialized: false
 }));
